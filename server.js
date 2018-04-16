826d6fd04fd38a74f834301f2671bedb95877873
mmacy <marsma@microsoft.com>
2018-04-16 14:53:14 -0700

    change server port 5000 to 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-var port = 5000
+var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
