87277c36a5fa12ba5dac983e56d8751bada970b3
t.milecki <t.milecki@gmail.com>
2022-03-13 16:25:56 +0100

    acr test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
+  response.write('Hello World!!!\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
